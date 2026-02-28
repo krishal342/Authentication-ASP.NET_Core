ac0e50c9c7a629fc52ccffd9e7daf2d305003f63
krishal <krishal342@gmail.com>
2026-02-28 18:42:02 +0545

    Creating User Model

---
+++ Models/User.cs
@@ -0,0 +1,19 @@
+﻿namespace Authentication.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; } = null!;
+
+        public string LastName { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+
+        public string Password { get; set; } = null!;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+
+    }
+}
